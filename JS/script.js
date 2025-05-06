bbbb5dc1fcb92fbe5bb4f5c9fc2083a142f75bc3
alinatheboss <alina.bosshard@stud.fhgr.ch>
2025-05-06 19:32:47 +0200

    Set-Up & Fetch
    aus Unterricht am 06.05.25

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+// Joke API
+async function randomjoke() {
+    const url = 'https://v2.jokeapi.dev/joke/Any?lang=de&type=single';
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const joke = await randomjoke();
+console.log(joke);
+
+
